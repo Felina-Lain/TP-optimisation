2489c6a65c1fd74d6e8a93ecbdc87293b3bacde8
Felina-Lain <sawula@free.fr>
2017-02-17 19:58:16 +0100

    generation procédural intégrée, mais y'a un bug

---
+++ TP3 code/Assets/Scripts/GenerationProce.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerationProce: MonoBehaviour {
+
+	public GameObject prefab1;
+	public GameObject prefab2;
+	public GameObject prefab3;
+
+	float timer;
+	public float timerMax;
+
+	// Use this for initialization
+	void SpawnRandom () {
+
+
+		float leftLimitation = Camera.main.ViewportToWorldPoint(new Vector3(0,0,0)).x;
+		float rightLimitation = Camera.main.ViewportToWorldPoint(new Vector3(1,0,0)).x;
+
+		float upLimitation = Camera.main.ViewportToWorldPoint(new Vector3(0,0,0)).y;
+		float downLimitation = Camera.main.ViewportToWorldPoint(new Vector3(0,1,0)).y;
+
+
+		int numeroaupif = Random.Range(1,4);
+
+		Vector3 randompos = new Vector3 (rightLimitation + 0.1f, Random.Range (downLimitation, upLimitation), 0);
+			
+		if(numeroaupif == 1)
+		{
+			GameObject inst = Instantiate (prefab1, randompos, Quaternion.identity);
+			inst.transform.parent = this.transform;
+		}
+		if(numeroaupif == 2)
+		{
+			GameObject inst = Instantiate(prefab2, randompos ,  Quaternion.identity);
+			inst.transform.parent = this.transform;
+		}
+		if(numeroaupif == 3)
+		{
+			GameObject inst = Instantiate(prefab3, randompos ,  Quaternion.identity);
+			inst.transform.parent = this.transform;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += 1 * Time.deltaTime;
+		if (timer >= timerMax) {
+			timer = 0f;
+			timerMax = Random.Range (2, 4);
+			SpawnRandom ();
+		}
+	}
+}
