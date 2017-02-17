63e0ec897c6ee837f55b9184e7a8e8efe96869ea
Farrand <rf.robert@yahoo.fr>
2017-02-17 16:58:37 +0100

    Static enemy behavior, joueur clampé aux bords de l'ecran

--- TP3 code/Assets/Scripts/ShipController.cs
+++ TP3 code/Assets/Scripts/ShipController.cs
@@ -12,10 +12,20 @@ public class ShipController : MonoBehaviour {
 
 	public static int _score;
 
+	float leftLimitation;
+	float rightLimitation;
+	float upLimitation;
+	float downLimitation;
+
     void Awake()
     {
         RBody2D = GetComponent<Rigidbody2D>();
 		manag =  Camera.main.GetComponent<Manager>();
+
+		leftLimitation = Camera.main.ViewportToWorldPoint(Vector3.zero).x;
+		rightLimitation = Camera.main.ViewportToWorldPoint(Vector3.right).x;
+		upLimitation = Camera.main.ViewportToWorldPoint(Vector3.zero).y;
+		downLimitation = Camera.main.ViewportToWorldPoint(Vector3.up).y;
     }
 
     void Update()
@@ -27,25 +37,34 @@ public class ShipController : MonoBehaviour {
         }
         float inputX = Input.GetAxis("Horizontal");
         float inputY = Input.GetAxis("Vertical");
-        bool blast = Input.GetButtonDown("Fire1");
 
         //Calcul movement
         Movement = new Vector2(Speed.x * inputX, Speed.y * inputY);
 
         //Blast
-        if (blast)
+		if (Input.GetButtonDown("Fire1"))
         {
             WeaponScript weapon = GetComponent<WeaponScript>();
 
             weapon.Attack(false); //false: player is not an enemy
 
         }
+
     }
 
     void FixedUpdate()  // appele a intervalle fix
     {
         RBody2D.velocity = Movement;
     }
+
+	void LateUpdate()
+	{
+		//clamp player in the screen
+		Vector3 _tempPos = transform.position;
+		_tempPos.x = Mathf.Clamp(_tempPos.x, leftLimitation, rightLimitation);
+		_tempPos.y = Mathf.Clamp(_tempPos.y, upLimitation, downLimitation);
+		transform.position = _tempPos;
+	}
 
     void OnDestroy()
     {
---
+++ TP3 code/Assets/StaticEnemyBehavior.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticEnemyBehavior : MonoBehaviour {
+
+	[Header("Proportion of the screen to freeze to")]
+	[Range(0f, 1f)]
+	public float screenPosX = 0.8f;
+
+	private float thisXViewportPos;
+
+	void Update () {
+
+		thisXViewportPos = Camera.main.WorldToViewportPoint(transform.position).x;
+
+
+		if(thisXViewportPos < screenPosX)
+		{
+			transform.parent = null;
+			this.enabled = false;
+			//C'est ici que l'on enclenche un wait puis un patern
+		}
+
+	}
+}
