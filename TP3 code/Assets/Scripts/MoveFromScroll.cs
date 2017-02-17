5c8b18eb5801b515fab15bdfc9c4a3cd5a9ffbe5
Felina-Lain <sawula@free.fr>
2017-02-17 22:40:56 +0100

    almost done, need replace the OnDestroy (and return score to 0 after pool creation) And check save again

---
+++ TP3 code/Assets/Scripts/MoveFromScroll.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveFromScroll : MonoBehaviour {
+
+	[Range(1f, 20f)]
+	public float speed = 6f;
+
+	public Vector3 dir = -Vector3.right;
+
+	public bool useFreezeOnScreenPos = false;
+
+	[Header("Proportion of the screen to freeze to (fom 0 to 1 (viewportSpace))")]
+	public Vector3 screenFreezePos = new Vector3(0.8f, 0f, 0f);
+
+	private Vector3 thisViewportPos;
+
+	void Update () {
+
+		transform.Translate(dir * speed * Time.deltaTime);
+
+		thisViewportPos = Camera.main.WorldToViewportPoint(transform.position);
+
+		if(useFreezeOnScreenPos)
+		{
+			if(thisViewportPos.x < screenFreezePos.x || thisViewportPos.y < screenFreezePos.y || thisViewportPos.z < screenFreezePos.z)
+			{
+				this.enabled = false;
+			}
+		}
+
+		if(thisViewportPos.x < 0f)
+		{
+			Destroy(gameObject);
+		}
+
+	}
+
+}
